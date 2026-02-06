f4a7bb7d3818549a671011003f68b72608ccad6f
nhajimammadova17226 <nhajimammadova17226@ada.edu.az>
2026-02-06 11:06:23 +0400

    Add Sum program to calculate the sum of two integers

---
+++ exercise1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first integer: ");
+
+        int num1 = input.nextInt();
+
+        System.out.println("Enter second integer: ");
+
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum = " + sum);
+
+        input.close();
+    }
+}
